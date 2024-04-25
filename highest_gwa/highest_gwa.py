71d4b2c0d38013914fda085d9564a82b6d5a7ca1
unknown <mangulabnanshervinmarco@gmail.com>
2024-04-26 01:57:38 +0700

    Find student with highest GWA

--- highest_gwa/highest_gwa.py
+++ highest_gwa/highest_gwa.py
@@ -8,7 +8,15 @@ def main():
     with open("student_gwa.txt", "r") as file:
         student_data = [line.strip().split(",") for line in file.readlines()]
 
-# Find student with highest GWA
+    # Find student with highest GWA
+    highest_gwa = 0
+    highest_gwa_student = ""
+    for student in student_data:
+        name, gwa = student[0], float(student[1])
+        if gwa > highest_gwa:
+            highest_gwa = gwa
+            highest_gwa_student = name
+
 # Output the name of the student with the highest GWA
 
 if __name__ == "__main__":
