464979db79f40783e72c2361b6ff2f3867c4e072
unknown <mangulabnanshervinmarco@gmail.com>
2024-04-26 02:31:27 +0700

    Function to write input lines to a file

--- write_to_text_file/write_to_text_file.py
+++ write_to_text_file/write_to_text_file.py
@@ -2,15 +2,23 @@
 #         text content and write them into a text file named mylife.txt.
 
 # Pseudocode
+
 # Function to write input lines to a file
-# Open the file in 'write' mode
-# Prompt the user to input a line
-line = input("Enter line: ")
+def write_to_file(filename):
+        
+    # Open the file in 'write' mode
+    with open(filename, 'w') as file:
+        while True:
+              
+              # Prompt the user to input a line
+              line = input("Enter line: ")
+              
+              # Write the line to the file
+              file.write(line + '\n')
+              
+              # Ask if there are more lines to add
+              more_lines = input("Are there more lines? (y/n): ").lower()
+              if more_lines != 'y':
+                  break
 
-# Write the line to the file
-file.write(line + '\n')
-
-# Ask if there are more lines to add
-more_lines = input("Are there more lines? (y/n): ").lower()
-            if more_lines != 'y':
-                break
+write_to_file('mylife.txt')
