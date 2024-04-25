702dd594b702eeea106a45274a694e2a28000366
unknown <mangulabnanshervinmarco@gmail.com>
2024-04-26 02:22:38 +0700

    Prompt the user to input a line

--- write_to_text_file/write_to_text_file.py
+++ write_to_text_file/write_to_text_file.py
@@ -1,13 +1,11 @@
-#Purpose: A method in Python that will create two separate text files after 
-#         reading the source text file named integers.txt, which contains 
-#         20 integers. The first output file will be named double.txt, 
-#         containing the square of all even integers found in integers.txt, 
-#         and the second file will be named triple.txt, containing the cube 
-#         of all odd numbers found in integers.txt.
+#Purpose: A program that will allow the user to input multiple lines of 
+#         text content and write them into a text file named mylife.txt.
 
-# Purpose
+# Pseudocode
 # Function to write input lines to a file
 # Open the file in 'write' mode
 # Prompt the user to input a line
+line = input("Enter line: ")
+
 # Write the line to the file
 # Ask if there are more lines to add
