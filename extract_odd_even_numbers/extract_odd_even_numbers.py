1f692b052a5267435404cef0a9fe92f1e81d9600
unknown <mangulabnanshervinmarco@gmail.com>
2024-04-26 01:44:31 +0700

    Write even numbers to even.txt

--- extract_odd_even_numbers/extract_odd_even_numbers.py
+++ extract_odd_even_numbers/extract_odd_even_numbers.py
@@ -14,5 +14,9 @@ def main():
     even_numbers = [num for num in numbers if num % 2 == 0]
     odd_numbers = [num for num in numbers if num % 2 != 0]
 
-# Write even numbers to even.txt
+    # Write even numbers to even.txt
+    with open("even.txt", "w") as even_file:
+        for num in even_numbers:
+            even_file.write(str(num) + "\n")
+
 # Write odd numbers to odd.txt
