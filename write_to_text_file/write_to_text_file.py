38ffc434e0d4d4c636face56a9d04e1fff506932
unknown <mangulabnanshervinmarco@gmail.com>
2024-04-26 00:52:45 +0700

    added purpose

---
+++ write_to_text_file/write_to_text_file.py
@@ -0,0 +1,6 @@
+#Purpose: A method in Python that will create two separate text files after 
+#         reading the source text file named integers.txt, which contains 
+#         20 integers. The first output file will be named double.txt, 
+#         containing the square of all even integers found in integers.txt, 
+#         and the second file will be named triple.txt, containing the cube 
+#         of all odd numbers found in integers.txt.
