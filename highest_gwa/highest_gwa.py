f14dbcc44048ecd19d4d461b7739c25e4bceed21
unknown <mangulabnanshervinmarco@gmail.com>
2024-04-26 01:15:43 +0700

    added pseudocode

--- highest_gwa/highest_gwa.py
+++ highest_gwa/highest_gwa.py
@@ -1,3 +1,9 @@
 # Purpose: A program that reads a file containing the names of 20 students along 
 #          with their GWA (Grade Weighted Average) and outputs the name of the 
 #          student who achieved the highest GWA, including their GWA score.
+
+# Pseudocode
+# Read number from numbers.txt
+# Extract even and odd numbers
+# Write even numbers to even.txt
+# Write odd numbers to odd.txt
