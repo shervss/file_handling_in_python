bbe52b34c00bd152087b44d5ce08f865c37207bc
unknown <mangulabnanshervinmarco@gmail.com>
2024-04-26 01:10:11 +0700

    added pseudocode

--- extract_odd_even_numbers/extract_odd_even_numbers.py
+++ extract_odd_even_numbers/extract_odd_even_numbers.py
@@ -2,3 +2,9 @@
 #          and create two other text files: even.txt, which will contain all the even 
 #          numbers extracted from numbers.txt, and odd.txt, which will contain all the 
 #          odd numbers extracted from numbers.txt.
+
+# Pseudocode
+# Read numbers from numbers.txt
+# Extract even and odd numbers
+# Write even numbers to even.txt
+# Write odd numbers to odd.txt
